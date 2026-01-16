10f0eb4f02ebcd3d7005799efb2960e46ba48b00
Nathanael Minoletti <nathanael.minoletti@swisslectures.ch>
2026-01-16 17:13:09 +0100

    add keystatic und test a bit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import markdoc from "@astrojs/markdoc";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [markdoc(), react(), keystatic()],
 });
